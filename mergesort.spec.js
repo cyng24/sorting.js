d64967b69bed4a98d2c7adb8934db8eadb4bf91e
Renee Sajedian <rsajedian@gmail.com>
2017-09-14 16:00:55 -0400

    mergesort solution

--- mergesort.spec.js
+++ mergesort.spec.js
@@ -4,12 +4,17 @@ describe('Split Array function', function() {
   });
 
   it('splits an array of one element', function() {
-    expect(split([2])).toEqual([[2]]);// your code here
+    expect(split([2])).toEqual([[2], []]);// your code here
   });
 
   it('also splits a long array', function() {
     expect(split([1, 2, 3, 4, 5, 6])).toEqual([[1, 2, 3], [4,5,6]]);
   })
+
+  it('also splits a long array', function() {
+    expect(split([6,2,8,4,9,1,5,7,3,0])).toEqual([[6, 2, 8, 4, 9], [1, 5, 7, 3, 0]]);
+  })
+
 });
 
 describe('Merge function', function() {
@@ -17,12 +22,39 @@ describe('Merge function', function() {
     expect(merge([2, 4], [3]) ).toEqual([2, 3, 4]);// your code here
   });
 
-  it('splits an array of one element', function() {
-    expect(split([2])).toEqual([[2]]);// your code here
+  it('merges an empty array with a single element array', function() {
+    expect(merge([2], [])).toEqual([2]);// your code here
   });
 
-  it('also splits a long array', function() {
-    expect(split([1, 2, 3, 4, 5, 6])).toEqual([[1, 2, 3], [4,5,6]]);
+  it('also merges a long array', function() {
+    expect(merge([2, 5, 6], [1, 3, 4])).toEqual([1, 2, 3, 4, 5, 6]);
   })
+});
+
+
+describe('Merge function', function() {
+  it('is able to merge two sorted arrays into one sorted array', function() {
+    expect(merge([2, 4], [3]) ).toEqual([2, 3, 4]);// your code here
+  });
+
+  it('merges an empty array with a single element array', function() {
+    expect(merge([2], [])).toEqual([2]);// your code here
+  });
+
+  it('also merges a long array', function() {
+    expect(merge([2, 5, 6], [1, 3, 4])).toEqual([1, 2, 3, 4, 5, 6]);
+  })
+});
+
+describe('Merge Sort', function(){
+
+it('sorts a larger array', function(){
+  expect( mergeSort([6,2,8,4,9,1,5,7,3,0]) ).toEqual( [0,1,2,3,4,5,6,7,8,9] );
+});
+
+it('handles repeated numbers', function(){
+  expect( mergeSort([4,4,2]) ).toEqual( [2,4,4] );
+});
+
 });
 
--- mergesosrt.js
+++ mergesosrt.js
@@ -1,9 +1,37 @@
 function split(wholeArray) {
     var center = Math.floor(wholeArray.length/2);
-    if (center === 0) return [wholeArray];
+    console.log('center of array', center);
+    if (center === 0) return [wholeArray, []];
     var firstHalf = wholeArray.slice(0, center);
     var secondHalf = wholeArray.slice(center);
+    //console.log('splitting', [firstHalf, secondHalf]);
     return [firstHalf, secondHalf];
   }
 
+function merge(arr1, arr2) {
+  console.log('arrays to be merged', arr1, arr2)
+  var result = [];
+  while(arr1.length > 0 && arr2.length > 0) {
+    if (arr1[0] <= arr2[0]) result.push(arr1.shift())
+    else result.push(arr2.shift())
+  }
+  result = result.concat(arr1).concat(arr2);
+  console.log('merge result', result)
+  return result
+
+}
+
+function mergeSort(arr) {
+  //console.log(arr);
+  console.log('array', arr);
+  if(arr.length < 2) return arr;
+  else {
+    console.log('array to split', arr);
+    var splitArr = split(arr);
+    console.log('splitArr', splitArr);
+
+    return merge(mergeSort(splitArr[0]), mergeSort(splitArr[1]));
+    console.log('merged array', merge(recurseOne, recurseTwo));
+  }
+}
 
